372efc3ad30ef6515296faa1d35c99b431ee1cef
Simon Duff <bastett@gmail.com>
2016-08-28 11:04:11 +0100

    Initial commit

---
+++ src/DdhpCore.Micros.Clubs/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace DdhpCore.Micros.Clubs
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
---
+++ src/DdhpCore.Micros.Runner/MicrosRuner.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace DdhpCore.Micros.Runner
+{
+    public class MicrosRuner
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public MicrosRuner()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void Run()
+        {
+            Task.Run(RunJobs, _cancellationTokenSource.Token);
+        }
+
+        private async Task RunJobs()
+        {
+            var cancellationToken = _cancellationTokenSource.Token;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // Read from queues here
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+
+        public void StopApplication()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
