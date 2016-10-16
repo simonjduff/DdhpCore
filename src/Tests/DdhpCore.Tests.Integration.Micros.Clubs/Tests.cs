5a197e39c83afef7b5d2735ee545f474254f6fd6
Simon Duff <bastett@gmail.com>
2016-10-16 12:19:32 +0100

    Setting up test project

---
+++ src/Tests/DdhpCore.Tests.Integration.Micros.Clubs/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace DdhpCore.Tests.Integration.Micros
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
