9642d9880436714707d51d8be577d9b223eb77ac
Simon Duff <simon.j.duff@gmail.com>
2016-10-26 09:40:33 +0100

    Setting up contracts import

---
+++ src/DdhpCore.Apps.LegacyDataImporter/LegacyModels/Contract.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LegacyDataImporter.LegacyModels
+{
+    [Table("Contracts", Schema = "ddhp")]
+    public class Contract
+    {
+        public int Id { get; set; }
+        public int FromRound { get; set; }
+        public int ToRound { get; set; }
+        public int PlayerId { get; set; }
+        public int TeamId { get; set; }
+        public int DraftPick { get; set; }
+    }
+}
---
+++ src/DdhpCore.Apps.LegacyDataImporter/Models/Contract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace LegacyDataImporter.Models
+{
+    public class Contract : TableEntity
+    {
+        public int FromRound { get; set; }
+        public int ToRound { get; set; }
+        public Guid PlayerId { get; set; }
+        public int DraftPick { get; set; }
+        public Guid ClubId { get; set; }
+    }
+}
