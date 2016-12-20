d9d0e0068c01ca0398ea0a44db8e83a63982b831
Simon Duff <simonjduff@users.noreply.github.com>
2016-12-20 14:09:08 +0000

    Retrieve played teams api (#7)

---
+++ src/DdhpCore.FrontEnd/Extensions/TableResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DdhpCore.FrontEnd.Extensions
+{
+    public static class TableResultExtensions
+    {
+        public static IActionResult ToActionResult(this TableResult result, ControllerBase controller)
+        {
+            switch ((HttpStatusCode)result.HttpStatusCode)
+            {
+                case HttpStatusCode.NotFound:
+                    return controller.NotFound();
+                default:
+                    return new ObjectResult(result.Result);
+            }
+        }
+    }
+}
