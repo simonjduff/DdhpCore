d64f0090fa5973cb56d78876690a58655cc04588
Simon Duff <simon.j.duff@gmail.com>
2017-01-12 21:24:41 +0000

    Serialize player in contract

--- src/DdhpCore.FrontEnd/Models/Api/ClubSeason.cs
+++ src/DdhpCore.FrontEnd/Models/Api/ClubSeason.cs
@@ -35,12 +35,12 @@ namespace DdhpCore.FrontEnd.Models.Api
             }
         }
 
-        private List<Contract> _contracts = new List<Contract>();
+        private List<Storage.Models.Contract> _contracts = new List<Storage.Models.Contract>();
 
         public string Contracts
         {
             get { return JsonConvert.SerializeObject(_contracts); }
-            set { _contracts = (List<Contract>)JsonConvert.DeserializeObject<List<Contract>>(value); }
+            set { _contracts = (List<Storage.Models.Contract>)JsonConvert.DeserializeObject<List<Storage.Models.Contract>>(value); }
         }
 
         public int Version { get; set; }
