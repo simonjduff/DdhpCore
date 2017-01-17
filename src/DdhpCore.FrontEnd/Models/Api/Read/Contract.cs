22c429227df86f9879e390575edd1f3988f23014
Simon Duff <simonjduff@users.noreply.github.com>
2017-01-17 14:06:55 +0000

    Missed files from last PR (#20)
    * Read club from read table
    
    * Missing files from last commit

---
+++ src/DdhpCore.FrontEnd/Models/Api/Read/Contract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DdhpCore.FrontEnd.Models.Api.Read
+{
+    public class Contract
+    {
+        public Contract(Guid playerId,
+        int fromRound,
+        int toRound,
+        int draftPick)
+        {
+            PlayerId = playerId;
+            FromRound = fromRound;
+            ToRound = toRound;
+            DraftPick = draftPick;
+        }
+        public Guid PlayerId { get; set; }
+        public int FromRound { get; set; }
+        public int ToRound { get; set; }
+        public int DraftPick { get; set; }
+        public Player Player { get; set; }
+    }
+}
---
+++ src/DdhpCore.FrontEnd/Models/Api/Read/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DdhpCore.FrontEnd.Models.Api.Read
+{
+    public class Player
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid CurrentAflClubId { get; set; }
+
+        public bool Active { get; set; }
+        public string FootywireName { get; set; }
+    }
+}
