badcaff4353b538cb2619ae8a4da602c5a4f197f
Simon Duff <simon.j.duff@gmail.com>
2017-01-17 17:06:30 +0000

    Work towards displaying stats page

--- src/DdhpCore.FrontEnd/Models/Api/Read/Contract.cs
+++ src/DdhpCore.FrontEnd/Models/Api/Read/Contract.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DdhpCore.FrontEnd.Models.Api.Read
 {
@@ -13,11 +15,13 @@ namespace DdhpCore.FrontEnd.Models.Api.Read
             FromRound = fromRound;
             ToRound = toRound;
             DraftPick = draftPick;
+            Stats = Enumerable.Empty<Stat>();
         }
         public Guid PlayerId { get; set; }
         public int FromRound { get; set; }
         public int ToRound { get; set; }
         public int DraftPick { get; set; }
         public Player Player { get; set; }
+        public IEnumerable<Stat> Stats { get; set; }
     }
 }
