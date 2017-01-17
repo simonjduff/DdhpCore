fbfea74c6806c57baece018ec72a6e6cec69058a
Simon Duff <simon.j.duff@gmail.com>
2017-01-17 17:06:30 +0000

    Work towards displaying stats page

---
+++ src/DdhpCore.FrontEnd/Models/Api/Read/Stat.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace DdhpCore.FrontEnd.Models.Api.Read
+{
+    public class Stat
+    {
+        [JsonProperty("rn")]
+        public int RoundNumber { get; set; }
+        [JsonProperty("f")]
+        public int Forward { get; set; }
+        [JsonProperty("m")]
+        public int Midfield { get; set; }
+        [JsonProperty("r")]
+        public int Ruck { get; set; }
+        [JsonProperty("t")]
+        public int Tackle { get; set; }
+    }
+}
